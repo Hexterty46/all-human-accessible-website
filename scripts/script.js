5a123b6a4839bc0ff363d33f9341e33492c808c4
Hexterty46 <efsuslusemih@gmail.com>
2025-10-29 12:42:49 +0100

    Alle weijzigingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,49 @@
 const fixedButton = document.getElementById('fixedButton');
 const chatBox = document.getElementById('chatBox');
-const closeChat = document.getElementById('closeChat');
+const closeChatButton = document.getElementById('closeChat');
 
-fixedButton.addEventListener('click', () => {
-    stickyButton.style.display = 'flex';
-    chatBox.style.display = 'none';
-})
 
-closeChat.addEventListener('click', () => {
-    fixedButton.style.display = 'none';
-    chatBox.style.display = 'flex';
-})
+const filterButton = document.getElementById('filterenButton')
+const filterenSection = document.getElementById('filterSection')
+
+fixedButton.addEventListener('click', openChat )
+closeChatButton.addEventListener('click', closeChat )
+
+filterButton.addEventListener('click', toggleFilter )
+// filterenSection.addEventListener('click', closeFilter )
+
+
+
+document.addEventListener('keydown', handleKeyPress);
+
+
+
+function handleKeyPress(event) {
+  if (event.code == 'Escape') {
+    closeChat();
+    closeFilter();
+  }
+}
+
+function openChat() {
+    chatBox.classList.add("is-open");
+    fixedButton.classList.add("is-open");
+}
+
+
+function closeChat() {
+    chatBox.classList.remove("is-open");
+    fixedButton.classList.remove("is-open");
+}
+
+
+
+function toggleFilter() {
+    filterenSection.classList.toggle("is-open")
+    // filterButton.classList.toggle("is-open")
+}
+
+// function closeFilter() {
+//     filterenSection.classList.remove("is-open")
+//     filterButton.classList.remove("is-open")
+// }
